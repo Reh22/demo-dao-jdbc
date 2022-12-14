0e7bd5678dff5370e519b8afe24fb50fa106d2d7
Renato <renatorenatex20@gmail.com>
2022-12-14 10:03:32 -0300

    Creating project and git repository

--- src/aplication/Progam.java
+++ src/aplication/Progam.java
@@ -3,6 +3,7 @@ package aplication;
 import java.util.Date;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Progam {
 
@@ -10,9 +11,9 @@ public class Progam {
 		
 		
 		Department obj = new Department(1, "Books");
-		
+	
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(obj);
+		System.out.println(seller);
 	}
 
 }
